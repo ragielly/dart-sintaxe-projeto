8c10d8c13e05e851f853473790f91897101bd7ec
ragielly <ragielly.19@gmail.com>
2025-10-12 21:58:18 -0300

    adicionando exercicios escritos

--- exercicios/bin/exercicios.dart
+++ exercicios/bin/exercicios.dart
@@ -1,5 +1,42 @@
-import 'package:exercicios/exercicios.dart' as exercicios;
 
-void main(List<String> arguments) {
-  print('Hello world: ${exercicios.calculate()}!');
+
+void main() {
+  //1) Calculando o dobro de um número - Faça um programa simples que calcula o dobro de um número inserido no terminal.
+  //2) Descobrindo a média de idades 
+    //Escreva um programa que calcula a idade média de três pessoas. O usuário deve ser capaz de inserir as três idades e, ao final, o programa exibe a média dos números.
+  //3)Desenvolva um programa que exibe uma pequena “ficha” com informações suas, utilizando os conhecimentos do Dart! As informações da ficha são:
+    // Nome;
+    // CPF;
+    // Idade;
+    // Altura;
+    // Participo da comunidade?
+  //4) Calculando o salário do freela
+    // Você foi contratado para um trabalho freelance. Crie um programa que lê as horas trabalhadas e calcula o salário líquido, considerando:
+
+    // A empresa paga 50 reais por hora trabalhada;
+    // O desconto do salário é de 5% e deve ser subtraído do salário bruto.
+  //5) Consertando erros em um programa
+    // Você está trabalhando em um programa de banco, que faz três ações:
+
+    // Mostrar o saldo bancário do usuário;
+    // Permitir que o usuário faça uma transferência pix (de forma simplificada);
+    // Subtrair o valor do pix do valor de saldo.
+  //6) Identificando e corrigindo tipos no Dart
+    // Chegou nas suas mãos um programa de retirada de pontos de um clube de benefícios.
+
+    // A lógica deve ser a seguinte:
+
+    // Os pontos iniciais começam no valor 100;
+    // O programa inicia com uma mensagem: “*Você tem [x] pontos.*”;
+    // Em seguida, exibe a mensagem: “*Quantos pontos você gostaria de resgatar?*”;
+    // O usuário digita quantos [y] pontos deseja retirar;
+    // O programa faz uma subtração (pontos iniciais [100] - pontos retirados [y]);
+    // Por fim, mostra uma mensagem que informa: “Você resgatou [y] pontos. Pontos restantes: [z].””
+  //7) Refatorando o código do clube de benefícios com função
+    //Se você resolveu o exercício anterior, agora refatore (modifique) o código, colocando a funcionalidade de subtração de pontos dentro de uma função.
+  //8) Implementando um programa de cupons
+    // Escreva um programa que informa quantos cupons um cliente ganhou ao fazer compras no mercado No Precinho. A regra da promoção é: a cada 50 reais gastos por compra, a pessoa recebe um cupom. O programa deve:
+
+    // Capturar a entrada com o valor gasto;
+    // Exibir mensagem de quantos cupons foram ganhos.
 }
