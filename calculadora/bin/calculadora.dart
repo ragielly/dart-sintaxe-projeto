67a17d106ad311658eeefdfd6b3a906bcd0c6c72
ragielly <ragielly.19@gmail.com>
2025-10-14 09:09:05 -0300

    adicionando validação nas variaveis de entrada

--- calculadora/bin/calculadora.dart
+++ calculadora/bin/calculadora.dart
@@ -2,9 +2,32 @@
 import 'dart:io';
 
 void main() {
-  double numeroUm = double.parse(stdin.readLineSync()!);
-  double numeroDois = double.parse(stdin.readLineSync()!);
-  String operacao = stdin.readLineSync()!;
+  double numeroUm = 0;
+  double numeroDois = 0;
+  String operacao = "";
+
+  String? entrada = stdin.readLineSync();
+  if(entrada!= null){
+    if(entrada!= ""){
+      numeroUm = double.parse(entrada);
+      print(" numero 1 : $numeroUm");
+    }
+
+  }
+
+ entrada = stdin.readLineSync();
+  if(entrada!= null){
+    if(entrada!= ""){
+      numeroDois = double.parse(entrada);
+      print(" numero 2 : $numeroDois");
+    }
+
+  }
+
+  entrada = stdin.readLineSync();
+  if(entrada != null){
+      operacao = entrada;    
+  }
 
 
   void soma(){
