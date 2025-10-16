d4d76ae2b887925f39e6e6267d09cfe5bdb7f3d1
ragielly <ragielly.19@gmail.com>
2025-10-16 09:26:49 -0300

    corrigindo exercicios e ajustes no nome do arquivo.

--- exercicios/bin/exercicios17.dart
+++ exercicios/bin/exercicios17.dart
@@ -1,8 +1,40 @@
-void main(){
   // 4) Criando a função de depósito e fazendo sua validação
   // Vamos continuar desenvolvendo o programa de banco.
   // Agora, concentre-se na operação de depósito e escreva uma função em que o usuário,
   // após digitar um valor numérico, o insere na conta. Lembre-se de validar se a entrada
-  // é um número positivo. Caso contrário, solicite novamente até que um valor válido seja fornecido.
+  // é um número positivo. Caso contrário, solicite novamente até que um valor válido seja fornecido. 
+import 'dart:io';
+
+void main(){
+
+  List<String> operacao = <String>["deposito", "retirada", "transferencia", "pagamento"];
+  String? op;
+  op = validarOperacao(op, operacao);
+  double? valor;
+  valor = validarValor(valor);
+  print("Operação escolhida: $op, valor: $valor");
   
 }
+
+  double? validarValor( double? val){
+    print("Digite um valor para operação :");
+    val = double.tryParse(stdin.readLineSync()!);
+    if(val == null || val <0 ){
+      print("Informe um valor valido");
+      return validarValor(val);
+    }else{
+      return val;
+    } 
+
+  }
+
+  String? validarOperacao( String? opcao , List<String> operacao){
+    print("Digite uma  operação [deposito,retirada,transferencia,pagamento]:");
+    opcao = stdin.readLineSync()!;
+    if(operacao.contains(opcao)){
+      return opcao;
+    }else{
+      print("Operação invalida, tente novamente");
+      return validarOperacao(opcao, operacao);
+    }
+  }
--- exercicios/bin/exercicios18.dart
+++ exercicios/bin/exercicios18.dart
@@ -4,3 +4,6 @@
   // Você está desenvolvendo uma plataforma de e-commerce onde a pessoa usuária pode escolher
   // entre diferentes métodos de pagamento como cartao, boleto, paypal e pix. Crie uma função
   // que solicite à pessoa usuária o método de pagamento desejado e valide se a entrada é válida, solicitando novamente em caso de erro.
+  void main(){
+    
+  }
