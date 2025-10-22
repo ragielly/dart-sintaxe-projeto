0a57c1cdb254863f2ae66f85c584bab4bebad929
ragielly <ragielly.19@gmail.com>
2025-10-22 15:09:17 -0300

    enviando exercicio resolvido

--- exercicios/bin/exercicios18.dart
+++ exercicios/bin/exercicios18.dart
@@ -2,3 +2,21 @@
 // Imprima, no console, os números de 1 a 5 com o laço for.
 // 2) Mostrando nomes no terminal
 // Crie uma lista de nomes que é exibida no terminal com o for. Os nomes podem ser: Ana, João e Maria.
+void main(){
+  imprimirNumero();
+  imprimirNome();
+}
+
+void imprimirNumero(){
+  for (var i=1; i<=5; i++ ) {
+    print(i);
+  }
+
+}
+
+void imprimirNome(){
+  List<String> nomes = <String>["Ana", "João" , "Maria"];
+  for (var i=0; i< nomes.length; i++) {
+    print(nomes[i]);
+  }
+}
